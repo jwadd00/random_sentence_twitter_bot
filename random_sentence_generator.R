565aecd631370b1fff4d6ed8988a265e2d0008da
Jake Waddle <jwaddle@firebend.com>
2024-07-31 07:00:25 -0500

    updating

--- random_sentence_generator.R
+++ random_sentence_generator.R
@@ -11,11 +11,11 @@ consumerSecret <- Sys.getenv("TWITTER_APP_CONSUMER_SECRET")
 accessToken <- Sys.getenv("TWITTER_APP_ACCESS_TOKEN")
 accessTokenSecret <- Sys.getenv("TWITTER_APP_ACCESS_TOKEN_SECRET")
 
-token <- rtweet::rtweet_bot(
-  api_key       = consumerKey,
-  api_secret    = consumerSecret,
-  access_token  = accessToken,
-  access_secret = accessTokenSecret
+bot <- rtweet_bot(
+  api_key = '7Le7jaqT2SS7UODgZ5XWudXvk',
+  api_secret = 'nlZ0TqQDckxjeQfzjMQ7cIh41U0hCR8qk2vFUq9PQipUDFYq98',
+  access_token = '276697001-IkmDMp5RrQ4j71rtSj8LRTxRhCo35XV7e73YPgX5',
+  access_secret = ' SQEjuIZ5xYyD5JpoKpPoyam0XzjeW6BBKI8nh770P4jTa'
 )
 
 # nouns
@@ -35,7 +35,24 @@ names <- lexicon::common_names
 post <- glue("Never underestimate the willingness of a ", sample(irregular_nouns, 1), " named ", sample(names, 1), " to ", sample(verbs, 1), ' ', 'a ', sample(irregular_nouns, 1),
              "\n\n[-_-]random sentence generator built by jake\n[-_-]version 0.1 platinum deluxe 3000 \n[-_-]using: R Code and GitHub Actions")
 
-rtweet::post_tweet(
-  status = post,
-  token = token
+post
+
+rtweet::tweet_post(post)
+
+client <- rtweet_client(client_id = 'R0hPSFZOVnNiWllSYzhmTkFGQVk6MTpjaQ', app = 'Little Analysis Thing')
+client_as(client)
+client_save(client)
+auth_as(bot)
+
+if (FALSE) {
+  # It requires the Oauth2.0 Authentication
+   tp_id <- tweet_post(post)
+   tweet_post()
+ }
+
+ rtweet_user(
+  client_id = 'R0hPSFZOVnNiWllSYzhmTkFGQVk6MTpjaQ',
+  client_secret = '3-kAksCU3bo-4-IR1P0UdBYAxs0imUpl3E4qyb20dsersJubCY'
 )
+
+tweet_post(paste0("My first tweet with #rtweet #rstats at ", Sys.time()))
